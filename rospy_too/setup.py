2898ff94891e97f3fd7b996b527504966acda420
Ryan Govostes <rgovostes+git@gmail.com>
2026-01-13 12:42:30 -0600

    Silence warnings about package.xml

--- rospy_too/setup.py
+++ rospy_too/setup.py
@@ -1,8 +1,15 @@
 from setuptools import find_packages, setup
 
+package_name = 'rospy_too'
+
 setup(
-    name='rospy_too',
+    name=package_name,
     version='1.0.0',
     packages=find_packages(),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['rclpy'],
 )
